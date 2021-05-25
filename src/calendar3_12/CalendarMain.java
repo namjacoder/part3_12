3eb94c017a0ef9deedfad039acf2eb02a83a4b5e
kim <kim@DESKTOP-N9MU2BT>
2021-05-25 22:35:20 +0900

    입력값 예외처리 추가 2

--- src/calendar3_12/CalendarMain.java
+++ src/calendar3_12/CalendarMain.java
@@ -5,32 +5,53 @@ import java.util.Scanner;
 public class CalendarMain {
 	
 	public static void main(String[] args) {
-		System.out.println("년도를 입력하세요.");
-		System.out.print("YEAR> ");
-		Scanner scanner = new Scanner(System.in);
-		
-		//년도 입력값이 int인지 체크
-		while(!scanner.hasNextInt()) {
-			scanner.next();
-			System.out.println("숫자를 사용해 년도를 입력하세요.");
-		}
-		
-		int nYear = scanner.nextInt();
-		
-		System.out.println("달을 입력하세요.");
-		System.out.print("MONTH> ");
-		Scanner scanner2 = new Scanner(System.in);
-		
-		//달 입력값이 int인지 체크
-		while(!scanner2.hasNextInt()) {
-			scanner2.next();
-			System.out.println("숫자를 사용해 월을 입력하세요.");
-		}
-		
-		int nMonth = scanner2.nextInt();
+		int nYear = 0;
 
-		String sStartDay = "";
+		while(true) {
+			System.out.println("년도를 입력하세요.");
+			System.out.print("YEAR> ");
+			Scanner scanner = new Scanner(System.in);
+			
+			//년도 입력값이 int인지 체크
+			while(!scanner.hasNextInt()) {
+				scanner.next();
+				System.out.println("숫자를 사용해 년도를 입력하세요.");
+			}
 		
+			nYear = scanner.nextInt();
+			
+			if(nYear < 1 || nYear > 2021) {
+				continue;
+			}else {
+				break;
+			}
+		}
+		
+		int nMonth = 0;
+
+		while(true) {
+			System.out.println("달을 입력하세요.");
+			System.out.print("MONTH> ");
+			Scanner scanner2 = new Scanner(System.in);
+			
+			//달 입력값이 int인지 체크
+			while(!scanner2.hasNextInt()) {
+				scanner2.next();
+				System.out.println("숫자를 사용해 월을 입력하세요.");
+			}
+			
+			nMonth = scanner2.nextInt();
+			
+			//1~12월 입력값만 받는다.
+			if(nMonth < 1 || nMonth > 12) {
+				continue;
+			}else {
+				break;
+			}
+		}
+		
+		String sStartDay = "";
+
 		//입력값 체크
 		while(true) {
 			System.out.println("첫번째 요일을 입력하세요. (SU, MO, TU, WE, TH, FR, SA)");
